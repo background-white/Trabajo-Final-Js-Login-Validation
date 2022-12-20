45880da70d1df692f93b2adc38210137bc3612b8
linkinkc <linkinkc@gmail.com>
2022-12-20 18:48:41 -0500

    actualizacion de login js

--- login.js
+++ login.js
@@ -11,10 +11,8 @@ const expresiones = {
 
 const campos = {
 	usuario: false,
-	// nombre: false,
 	password: false,
-	// correo: false,
-	// telefono: false
+
 }
 
 const validarFormulario = (e) => {
@@ -22,22 +20,10 @@ const validarFormulario = (e) => {
 		case "usuario":
 			validarCampo(expresiones.usuario, e.target, 'usuario');
 		break;
-		// case "nombre":
-		// 	validarCampo(expresiones.nombre, e.target, 'nombre');
-		// break;
 		case "password":
 			validarCampo(expresiones.password, e.target, 'password');
-			validarPassword2();
 		break;
-		// case "password2":
-		// 	validarPassword2();
-		// break;
-		// case "correo":
-		// 	validarCampo(expresiones.correo, e.target, 'correo');
-		// break;
-		// case "telefono":
-		// 	validarCampo(expresiones.telefono, e.target, 'telefono');
-		// break;
+
 	}
 }
 
@@ -59,26 +45,6 @@ const validarCampo = (expresion, input, campo) => {
 	}
 }
 
-// const validarPassword2 = () => {
-// 	const inputPassword1 = document.getElementById('password');
-// 	// const inputPassword2 = document.getElementById('password2');
-
-// 	if(inputPassword1.value !== inputPassword2.value){
-// 		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-incorrecto');
-// 		document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-correcto');
-// 		document.querySelector(`#grupo__password2 i`).classList.add('fa-times-circle');
-// 		document.querySelector(`#grupo__password2 i`).classList.remove('fa-check-circle');
-// 		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.add('formulario__input-error-activo');
-// 		campos['password'] = false;
-// 	} else {
-// 		document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-incorrecto');
-// 		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-correcto');
-// 		document.querySelector(`#grupo__password2 i`).classList.remove('fa-times-circle');
-// 		document.querySelector(`#grupo__password2 i`).classList.add('fa-check-circle');
-// 		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.remove('formulario__input-error-activo');
-// 		campos['password'] = true;
-// 	}
-// }
 
 inputs.forEach((input) => {
 	input.addEventListener('keyup', validarFormulario);
